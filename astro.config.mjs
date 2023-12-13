03711e6d6982fef4a539e292db597020cfe83e96
Dawid Kobierski <dawid.kobierski@gmail.com>
2023-12-13 20:13:15 +0100

    repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config';
+// import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  // adapter: node({ mode: 'standalone' }),
+  // build: {
+  //   assetsPrefix: 'https://cdn.example.com/'
+  // },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/entry.[hash].mjs',
+          chunkFileNames: 'assets/chunks/chunk.[hash].mjs',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
+});
